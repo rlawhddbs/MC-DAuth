c28b6a406ed9fcb93aca63ab375966c33d1b3eb2
김종윤 <rlawhddbs@dgsw.hs.kr>
2024-02-27 13:21:23 +0900

    :truck: :: rename project

---
+++ src/main/kotlin/kr/hs/dgsw/dauth/DAuth.kt
@@ -0,0 +1,15 @@
+package kr.hs.dgsw.dauth
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class DAuth : JavaPlugin() {
+
+    override fun onEnable() {
+
+    }
+
+    override fun onDisable() {
+
+    }
+
+}
