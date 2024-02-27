0b60b2d55d9dcf02824cff168dddeeb69254d3c2
김종윤 <rlawhddbs@dgsw.hs.kr>
2024-02-27 12:30:51 +0900

    :tada: :: Minecraft DAuth Plugin Development Start!

---
+++ build.gradle.kts
@@ -0,0 +1,45 @@
+plugins {
+    id("java")
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+    kotlin("jvm")
+}
+
+group = "kr.hs.dgsw"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven(url = "https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+    maven {
+        name = "codemc-snapshots"
+        url = uri("https://repo.codemc.io/repository/maven-snapshots/")
+    }
+}
+
+dependencies {
+    compileOnly("org.spigotmc:spigot-api:1.19.4-R0.1-SNAPSHOT")
+    implementation("net.wesjd:anvilgui:1.9.2-SNAPSHOT") // Anvil GUI
+
+    compileOnly("org.projectlombok:lombok:1.18.28") // lombok (설치하기 싫었는데 ㅠㅠ)
+    annotationProcessor("org.projectlombok:lombok:1.18.28")
+}
+
+tasks.shadowJar {
+    minimize {
+        exclude(dependency("net.wesjd:anvilgui:.*"))
+    }
+}
+
+tasks.register("copyPlugin", Copy::class) {
+    dependsOn("shadowJar")
+    from("build/libs/DAuth-1.0-SNAPSHOT-all.jar")
+    into("/Users/rlawhddbs/Desktop/minecraft-server/plugins")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(17)
+}
---
+++ src/main/java/kr/hs/dgsw/dauthplugin/DAuthPlugin.kt
@@ -0,0 +1,15 @@
+package kr.hs.dgsw.dauthplugin
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class DAuthPlugin : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+
+}
